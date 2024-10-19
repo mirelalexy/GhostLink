435ca79029970a9a74cb8df2bdaf730ae118635e
ale <tomescualexandramirela@gmail.com>
2024-10-19 11:36:55 +0300

    fix comments

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'; // For the icon in the button
 
 export default function App() {
   return (
@@ -10,13 +9,13 @@ export default function App() {
 
       {/* Map Container */}
       <View style={styles.mapContainer}>
-        {/* Here, you'll eventually render your map */}
+        {/* render map */}
       </View>
 
       {/* Danger Button */}
       <TouchableOpacity style={styles.dangerButton}>
       <Image
-          source={require('../../assets/images/danger-icon.png')} // Import and use the PNG
+          source={require('../../assets/images/danger-icon.png')}
         />
       </TouchableOpacity>
     </View>
@@ -26,30 +25,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4B0082', // Purple background color
-    alignItems: 'center', // Center the logo horizontally
+    backgroundColor: '#4B0082',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   logoText: {
-    fontSize: 32, // Larger font size for the logo
+    fontSize: 32,
     fontWeight: 'bold',
     color: 'white',
     position: 'absolute',
-    top: 65, // Adjust this to move the logo to the top
+    top: 65,
   },
   mapContainer: {
     width: 375,
     height: 375,
-    backgroundColor: '#D3D3D3', // Light gray background as a placeholder
-    borderRadius: 10, // Slight rounded corners
+    backgroundColor: '#D3D3D3',
+    borderRadius: 10,
     marginVertical: 20,
   },
   dangerButton: {
     position: 'absolute',
     bottom: 40,
     right: 30,
-    backgroundColor: '#9F1D1D', // Red background color for the button
-    borderRadius: 10, // Round button
+    backgroundColor: '#9F1D1D',
+    borderRadius: 10,
     width: 65,
     height: 65,
     alignItems: 'center',
